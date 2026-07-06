6a7a3887bd7e5967d01e46dc2b047ea69dd660d0
Mkhaled96 <mohamedkhaleedhussein@gmail.com>
2026-07-06 05:02:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
+import ClientDetail from "./pages/ClientDetail.jsx";
 import ClientPortal from "./pages/ClientPortal.jsx";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/clients/:clientId" element={<ClientDetail />} />
         <Route path="/client" element={<ClientPortal />} />
       </Routes>
     </BrowserRouter>
