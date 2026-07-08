70e3ce1b22b4eaa62878b8d621ad3b0fcd796ffe
Mkhaled96 <mohamedkhaleedhussein@gmail.com>
2026-07-08 23:49:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { LanguageProvider } from "./lib/i18n.jsx";
+import { ThemeProvider } from "./lib/theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
